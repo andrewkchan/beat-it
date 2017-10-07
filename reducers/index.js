c8d8404581836be52ccba4581914044a0c974d57
Andrew Chan <andrewkchan@berkeley.edu>
2017-10-06 23:08:19 -0700

    redux

---
+++ reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+import * as types from "../constants/ActionTypes";
+// import croplist from "../reducers/croplist";
+// import entities from "../reducers/entities";
+// import scope from "../reducers/scope";
+
+const initialState = {
+    captured: false,
+    image: null
+};
+
+// const rootReducer = combineReducers({
+//     croplist,
+//     entities,
+//     scope
+// });
+
+export default rootReducer;
